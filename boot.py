3b811517ca5c71a4de164663a4547b81be42440e
OCEAN OF ANYTHING <oceanofanything@gmail.com>
2022-03-18 17:07:26 +0530

    Bootloader Support And Boot Handler

--- boot.py
+++ boot.py
@@ -1,12 +1,16 @@
 from board import *
 import digitalio
 import storage
-
+# Naming The Usb Drive
+#storage.remount("/", readonly=False)
+#m = storage.getmount("/")
+#m.label = "USB Rubber Ducky"
+#storage.remount("/", readonly=True)
+#storage.enable_usb_drive()
 noStorageStatus = False
 noStoragePin = digitalio.DigitalInOut(GP15)
 noStoragePin.switch_to_input(pull=digitalio.Pull.UP)
 noStorageStatus = not noStoragePin.value
-
 if(noStorageStatus == True):
     # don't show USB drive to host PC
     storage.disable_usb_drive()
